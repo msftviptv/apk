cc960eefb812850b125bacdc312d27c3f5a44d64
msftviptv <64074648+msftviptv@users.noreply.github.com>
2020-08-08 10:44:06 +0200

    Add files via upload

---
+++ intro/egy.php
@@ -0,0 +1,2 @@
+{"Kategory":[ 
+{"cat_id":888, "kategory":"Channels", "ulke_flag":"http://213.128.89.165/Main/Flag/owl_bayrak/egypt.png", "ulke":"Egypt", "cat_url":"http://213.128.89.165/TEMEL/Primavera/egypt/kanal_egypt.txt", "cat_foto":"http://www.freelogovectors.net/wp-content/uploads/2013/02/TV-Icon-9.png"}]}
